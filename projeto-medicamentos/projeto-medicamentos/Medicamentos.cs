b0f0291923a9bcfa223ed35a69a60de29dc5e43e
Stiven Richardy <stivenrichardy445@gmail.com>
2025-11-01 14:03:51 -0300

    Initialize project structure and class skeletons

---
+++ projeto-medicamentos/projeto-medicamentos/Medicamentos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_medicamentos
+{
+    internal class Medicamentos
+    {
+        private List<Medicamento> listaMedicamentos;
+
+        public List<Medicamento> ListaMedicamentos { get => listaMedicamentos; set => listaMedicamentos = value; }
+
+        public Medicamentos()
+        {
+            ListaMedicamentos = new List<Medicamento>();
+        }
+
+        public void Adicionar(Medicamento medicamento)
+        {
+
+        }
+
+        public bool Deletar(Medicamento medicamento)
+        {
+            return false;
+        }
+
+        public Medicamento Pesquisar(Medicamento medicamento)
+        {
+            return medicamento;
+        }
+    }
+}
